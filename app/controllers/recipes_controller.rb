a79ffa12222895322a235e78d550f98d0b6a3411
Rachelle Keblitis <rkeblitis@gmail.com>
2014-11-06 15:20:58 -0800

    add edit, update, and destroy methods for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,12 +10,39 @@ class RecipesController < ApplicationController
 
   def create
 
-    if Recipe.create(params.require(:recipe).permit(:name, :description))
+    if Recipe.create(recipe_params)
       redirect_to root_path
     end
   end
 
   def show
+    find_recipe
+  end
+
+  def edit
+    find_recipe
+  end
+
+  def update
+    if @recipe = find_recipe.update(recipe_params)
+      redirect_to recipe_path
+    end
+  end
+
+  def destroy
+    if find_recipe.destroy
+      redirect_to recipes_path
+    end
+  end
+
+  private
+
+  def find_recipe
     @recipe = Recipe.find(params[:id])
   end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
+
 end
